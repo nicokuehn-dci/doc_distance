fd427cbd11b55e7d4d00caeddc054a44b5581b53
Auto Commit <devnull@example.com>
2025-11-04 15:05:13 +0100

    chore: remove temporary debug scripts

--- check_api.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.test import APIClient
-from django.urls import reverse
-
-c = APIClient()
-res = c.post(reverse('word-frequencies'), {"payload": ["h", "e", "l", "l", "o"]}, format='json')
-print('status:', res.status_code)
-print('content-type:', res['Content-Type'])
-print('content:')
-print(res.content.decode())
--- check_serializer.py
+++
@@ -1,9 +0,0 @@
-from distance.serializers import WordFreqSerializer
-
-s = WordFreqSerializer(data={"payload": ["h", "e", "l", "l", "o"]})
-print('is_valid:', s.is_valid())
-print('errors:', s.errors)
-from distance.serializers import WordFreqSerializer
-s = WordFreqSerializer(data={payload: [h,e,l,l,o]})
-print('is_valid:', s.is_valid())
-print('errors:', s.errors)
